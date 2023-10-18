37fbb8c0e027568a292558991d01f8d82b9d9f78
Mehedi Hasan <devbytemehedi@gmail.com>
2023-10-19 02:15:13 +0600

    feat: create BrandProvider.jsx

---
+++ src/context/BrandProvider.jsx
@@ -0,0 +1,16 @@
+
+import PropTypes from 'prop-types';
+
+const BrandProvider = props => {
+  return (
+    <div>
+      
+    </div>
+  );
+};
+
+BrandProvider.propTypes = {
+  
+};
+
+export default BrandProvider;
