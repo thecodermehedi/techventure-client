c2635558d1859e09ccb4216c53677f034b41c0b1
Mehedi Hasan <devbytemehedi@gmail.com>
2023-10-18 21:57:59 +0600

    feat: create Loading.jsx

---
+++ src/components/Loading/Loading.jsx
@@ -0,0 +1,9 @@
+const Loading = () => {
+  return (
+    <div className="flex justify-center items-center h-screen bg-slate-300 dark:bg-black">
+        <span className="loading loading-spinner loading-lg text-main"></span>
+      </div>
+  );
+};
+
+export default Loading;
