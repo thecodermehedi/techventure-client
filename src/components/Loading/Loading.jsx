2e3a3eaefc7230ce19cf751656044073f6ec6662
Mehedi Hasan <devbytemehedi@gmail.com>
2023-10-19 21:31:37 +0600

    update Loading.jsx

--- src/components/Loading/Loading.jsx
+++ src/components/Loading/Loading.jsx
@@ -1,8 +1,8 @@
 const Loading = () => {
   return (
-    <div className="flex justify-center items-center h-screen bg-slate-300 dark:bg-black">
-        <span className="loading loading-spinner loading-lg text-main"></span>
-      </div>
+    <div className="flex justify-center items-center h-screen bg-white dark:bg-black">
+      <span className="loading loading-spinner loading-lg text-main"></span>
+    </div>
   );
 };
 
